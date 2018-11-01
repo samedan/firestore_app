cd1d330880c0e6460fb295a45dd6b4ba4656c6b7
samedan <same.dan@gmail.com>
2018-11-01 10:48:05 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyD4bKC2k0NtsxBLXIT227f1pRn9l81l85c',
-    authDomain: 'fir-59.firebaseapp.com',
-    databaseURL: 'https://fir-59.firebaseio.com',
-    projectId: 'fir-59',
-    storageBucket: 'fir-59.appspot.com',
-    messagingSenderId: '571118115219'
+    
   }
 };
